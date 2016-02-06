135593b3fb8489b565ec702afc019f4afbca31fe
Kent Salcedo <kentsalcedo@gmail.com>
2016-02-06 11:24:41 -1000

    Adds a controller to index.html, adds controller to myApp.js with $scope dependency, and tests

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,7 +1,7 @@
 // this defines module since it is taking 2 arguments
-angular.module('myApp', []); // external dependencies go inside the []
+// external dependencies go inside the []
+angular.module('myApp', []);
 
-//
 var myApp = angular.module('myApp');
 
 myApp
@@ -10,3 +10,15 @@ myApp
 }).run(function(){
   // initialize
 });
+
+// basic controller, inside the function arguements, you put dependencies
+// first arguement is the name of the controller and function
+// $ is an angular created code
+// $scope adds scope dependency
+
+// you can put multiple dependencies
+// per angular docs, pass an array instead of a function
+
+myApp.controller('myController', ['$scope', function($scope){
+  $scope.myFirstName = "Kent";
+}]);
