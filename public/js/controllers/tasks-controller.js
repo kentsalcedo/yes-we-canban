d806fd43e91b59fa2f085e91d0d3d80e859cfd79
RizOrt218 <rizhiafalen@gmail.com>
2016-02-08 21:07:55 -1000

    adds function to move tasks when clicked

--- public/js/controllers/tasks-controller.js
+++ public/js/controllers/tasks-controller.js
@@ -2,6 +2,11 @@
 
 angular.module('myApp')
 .controller('TaskController', ['$scope','TaskService',
-  function($scope, TaskService){
-  $scope.TaskService = TaskService;
+  function ($scope, TaskService, MoveTask) {
+    $scope.TaskService = TaskService;
+
+    $scope.MoveTask = function (task) {
+      task.status += 1;
+      // console.log("task", task.title);
+  };
 }]);
--- public/js/services.js
+++ public/js/services.js
@@ -3,43 +3,50 @@
 // "use strict"
 
   function TaskService(){
-    this.tasks = [
-      {
-        id : 1,
-        title : 'get er done',
-        desc : 'hello, hell yaaa mudddaaaaa',
-        priority : 'supa low',
-        createdBy : 'manang Riz',
-        assignedTo : 'Boyette-Boy'
-      }
+    this.toDo = [
+      // {
+      //   id : 1,
+      //   title : 'get er to do',
+      //   desc : 'hello, hell yaaa mudddaaaaa',
+      //   priority : 'supa low',
+      //   createdBy : 'manang Riz',
+      //   assignedTo : 'Boyette-Boy',
+      //   status : 1
+      // }
     ];
 
-    this.getTasks = function(){
-      return this.tasks;
+    this.getToDoTasks = function(){
+      return this.toDo;
     };
 
-    this.getTask = function(id){
-      return this.tasks.filter(function(task){ // returns array of tasks
+    //moves tasks from to-do to in progress column
+    this.moveToInProgress = function () {
+      console.log("consoleLogging", this);
+    };
+
+
+    this.getToDoTask = function (id){
+      return this.toDo.filter(function (task) { // returns array of tasks
         return task.id === id; // boolean
       })
-      .reduce(function(_,task){ // _ means you don't care about the first arg
+      .reduce(function (_,task) { // _ means you don't care about the first arg
         return task;
       });
     };
 
-    this.addTask = function(task){
-      var nextId = this.tasks.length + 1;
+    this.addToDoTask = function (task){
+      var nextId = this.toDo.length + 1;
       task.id = nextId;
-      this.tasks.push({
+      this.toDo.push({
         // id : 1,
         title : task.title,
         desc : task.desc,
         priority : task.priority,
         createdBy : task.createdBy,
-        assignedTo : task.assignedTo
+        assignedTo : task.assignedTo,
+        status : 1
       });
     };
-
   } // end of this function
 
   angular.module('myApp')
