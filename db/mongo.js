49f069f3734cb1d9ae2670b6cea02e418e75cbd4
Kent Salcedo <kentsalcedo@gmail.com>
2016-02-10 09:18:51 -1000

    WIP

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,12 @@
 var Mongoose = require('mongoose');
 
-Mongoose.connect('mongodb://localhost/yes-we-canban');
+Mongoose.connect('mongodb://localhost/tasksDataBase');
 var db = Mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+
+db.once('open', function(){
+  console.log("connected");
+});
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,33 +1,59 @@
-"use strict";
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3001;
-// const Mongoose = require('./db/mongo');
-const Mongoose = require('./db/task.json');
+var express    = require('express');
+var app        = express();
+var PORT       = process.env.PORT || 3001;
+var db         = require('./db/mongo');
+var bodyParser = require('body-parser');
+var Mongoose   = require('mongoose');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var tasksSchema = Mongoose.Schema ({
+  title     : String,
+  desc      : String,
+  priority  : String,
+  createdBy : String,
+  assignedTo: String,
+  status    : String
+});
+
+var todos = Mongoose.model('todos', tasksSchema);
 
 app.use(express.static('./public'));
 
 app.get('/api', function (req, res) {
-  res.json(Mongoose);
-  console.log("consoleLogging222", Mongoose);
+  // var taskObject = todos.findOne();
+  // console.log("consoleLogging", taskObject);
+  todos.find(function (err, data) {
+    if (err) return console.error(err);
+    res.json(data);
+  });
 });
 
-// app.post('/api/add', function (req, res) {
-//   // console.log("consoleLogging", req.title);
-//   res.json(Mongoose);
-// });
+app.post('/api/add', function (req, res) {
+  res.redirect('/');
 
-// app.put('/api/id/tasks', function (req, res) {
-//   // console.log("consoleLogging", req.title);
-//   res.json(Mongoose);
-// });
+  return new todos({
+    title     : req.body.title,
+    desc      : req.body.desc,
+    priority  : req.body.priority,
+    createdBy : req.body.createdBy,
+    assignedTo: req.body.assignedTo,
+    status    : req.body.status
+  }).save();
+});
 
-// app.delete('/api/id/tasks', function (req, res) {
-//   // console.log("consoleLogging", req.title);
-//   res.json(Mongoose);
-// });
+app.put('/api/update', function (req, res) {
+  return todos.findOneAndUpdate({ _id : req.body._id },
+    { $set : res }, { new: true }, function(){
+      console.log("updated");
+    });
+});
+
+app.delete('/api/delete', function (req, res) {
+  // res.redirect('/');
+  return todos.find({ _id: req.body._id}).remove().exec();
+});
 
 app.get('*', function(req,res) {
  res.sendFile('/public/index.html', { root : __dirname });
