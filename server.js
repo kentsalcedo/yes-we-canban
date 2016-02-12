8ce89b8d5eec572518d30c190a66a80ab9369e99
Kent Salcedo <kentsalcedo@gmail.com>
2016-02-11 14:57:40 -1000

    Fixes 2 way binding of data for edit. Edit is working

--- server.js
+++ server.js
@@ -56,7 +56,6 @@ app.delete('/api/delete/:id', function (req, res) {
 
 app.put('/api/update', function(req, res) {
   console.log('server',req.body);
-
   todos.update(
     { _id : req.body._id },
     { $set : req.body },
