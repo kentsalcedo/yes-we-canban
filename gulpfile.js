dc386c2188b518475dc570f37659129fbeb96ca1
RizOrt218 <rizhiafalen@gmail.com>
2016-02-13 14:20:44 -1000

    installed nodemon gulp livereload dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect= require('gulp-connect');
+var nodemon= require('gulp-nodemon');
+
+gulp.task('start', function () {
+  nodemon({
+    script : 'server.js'
+  });
+});
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
+});
 
 gulp.task('sass', function() {
    gulp.src('sass/styles.scss')
@@ -7,8 +22,14 @@ gulp.task('sass', function() {
        .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('watch', function() {
-   gulp.watch('sass/**/*.scss',['sass']);
+gulp.task('livereload', function (){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
 });
 
-gulp.task('default', ['sass','watch']);
+gulp.task('watch', function() {
+   gulp.watch('sass/**/*.scss',['sass']);
+   gulp.watch('./public/**/*', ['livereload']);
+});
+
+gulp.task('default', ['connect','sass','watch', 'start']);
