b2410752a43af26fdcb49cd9588ccbdb8b5f24b0
Kent Salcedo <kentsalcedo@gmail.com>
2016-02-15 11:02:13 -1000

    changes config

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 app.use(session({
-  "secret" : secret,
+  // "secret" : secret,
   "saveUninitialized" : true,
   "resave" : true
 }));
