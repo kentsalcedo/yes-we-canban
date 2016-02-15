f3aef3e53e6b9778ca836dc2f3895f334867bc8b
Kent Salcedo <kentsalcedo@gmail.com>
2016-02-15 11:09:56 -1000

    Updates secret key path for server config

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 app.use(session({
-  // "secret" : secret,
+  "secret" : SECRET,
   "saveUninitialized" : true,
   "resave" : true
 }));
